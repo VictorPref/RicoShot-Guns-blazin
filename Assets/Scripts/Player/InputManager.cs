eb7394411277331b452467b747e18198fba6ba8a
VictorPref <VictorPref@users.noreply.github.com>
2018-08-30 13:27:46 -0400

    Input and Player Control
    Rev: Christian
    
    Player Control : 8 |  Player Control: 1 | Input System : 9 | Player Control : 2
    
    Added Update and FixedUpdate in GameFlow
    Added all Input for the players
    Player can change between Phase 1 and 2
    Player can Rotate
    Player can Shoot, no bullet created

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -4,29 +4,93 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
 
-    public InputPkg GetKeysInput()
+    bool rtDown = false;
+    public InputPkg GetKeysInput(int id)
     {
         //Create package
         InputPkg toRet = new InputPkg();
+        
 
         //Get input
-        float x = Input.GetAxis("Horizontal");
-        bool isFiring = Input.GetAxis("Fire1") > 0;
+        float left_horizontal = Input.GetAxis("P"+id+"_Horizontal");
+        float left_vertical= Input.GetAxis("P" + id + "_Vertical");
+        float right_horizontal = Input.GetAxis("P" + id + "_ROTATE");
+        float A= Input.GetAxis("P" + id + "_A");
+        float B= Input.GetAxis("P" + id + "_B");
+        float X= Input.GetAxis("P" + id + "_X");
+        float Y= Input.GetAxis("P" + id + "_Y");
+        float start= Input.GetAxis("P" + id + "_START");
+        float rb= Input.GetAxis("P" + id + "_RB");
+        float lb= Input.GetAxis("P" + id + "_LB");
+        float rt= Input.GetAxis("P" + id + "_RT");
+        float lt= Input.GetAxis("P" + id + "_LT");
 
+        
+      
         //Fill the package
-        toRet.dirPressed = new Vector2(x, 0);
-        toRet.shootPressed = isFiring;
+        toRet.leftDir = new Vector2(left_horizontal, left_vertical);
+        toRet.rightDir = new Vector2(right_horizontal, 0);
+        toRet.A = A != 0 ? true:false;
+        toRet.B = B != 0 ? true : false;
+        toRet.X = X != 0 ? true : false;
+        toRet.Y = Y != 0 ? true : false;
+        toRet.start = start != 0 ? true : false;
+        toRet.rb = rb != 0 ? true : false;
+        toRet.lb = lb != 0 ? true : false;
+
+
+        toRet.rt = rt != 0 ? true : false;
+
+        //Check if the input is already pressed so player can't spam the bullet
+        IsShooting(ref toRet.rt);
+
+        toRet.lt = lt != 0 ? true : false;
+        toRet.id = id;
 
         //return package
+        Debug.Log(toRet.ToString());
+
         return toRet;
+    }
+
+    public void IsShooting(ref bool rt)
+    {
+        if (rt && !rtDown)
+        {
+            rtDown = true;
+        }
+        else if (rtDown && rt)
+        {
+            rt = false;
+        }
+        else
+        {
+            rtDown = false;
+        }
     }
 
 
     public class InputPkg
     {
-        public Vector2 dirPressed;
-        public bool shootPressed;
+        public Vector2 leftDir;
+        public Vector2 rightDir;
+        public bool A;
+        public bool B;
+        public bool X;
+        public bool Y;
+        public bool start;
+        public bool rb;
+        public bool lb;
+        public bool rt;
+        public bool lt;
+        public int id;
 
+        override
+        public string ToString()
+        {
+            return "ID: " + id + " RB: " + rb + " RT: " + rt + " LB: " + lb + " LT: " + lt;
+        }
+        
     }
 
 }
