0c15f9b8b12e2d8129c6a843753df43b3e4c365d
VictorPref <VictorPref@users.noreply.github.com>
2018-08-31 12:34:38 -0400

    new Bullet system and Inventory build
    Rev: Christian
    
    Bullet - Physics : 23 | Bullet - Gfx : 24 | Framework : 40
    
    Bullet don't bounce correctly  but it's better than before. Inventory and InventoryManager created

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -11,25 +11,54 @@ public class Bullet : MonoBehaviour {
 
     public void initialization()
     {
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * 10;
+    //    rb.velocity = transform.right * vitesse;
     }
     public void UpdateBullet()
     {
-        currDir = new Vector3(transform.position.x, transform.position.y, 0);
+        //  currDir = new Vector3(transform.position.x, transform.position.y, 0).normalized;
+
+        transform.position += transform.right * vitesse * Time.deltaTime;
+
+
+        Ray2D ray = new Ray2D(transform.position, transform.right);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 20f);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Target Position: " + hit.point);
+            //  transform.position = hit.point;
+
+            if (transform.position.x == hit.point.x && transform.position.y == hit.point.y)
+            {
+                transform.Rotate(new Vector3(0, 0, 90));
+            }
+
+
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector3 newDir = new Vector3(transform.position.x, transform.position.y, 0);
-        float newDirValue = Mathf.Atan2(newDir.y - currDir.y, newDir.x - currDir.x);
-        float newDirValueDeg = (180 / Mathf.PI) * newDirValue;
-        transform.rotation = Quaternion.Euler(0, 0, newDirValueDeg);
 
-        Vector2 inDirection = transform.position.normalized;
-        Vector2 inNormal = collision.contacts[0].normal;
-        Vector2 newVelocity = Vector2.Reflect(inDirection, inNormal);
+        nb_rebond--;
+        if (nb_rebond == 0)
+            Destroy(gameObject);
 
-        rb.velocity = transform.right * vitesse;
+
+      
+
+        //Vector2 inDirection = transform.position.normalized;
+        //Vector2 inNormal = collision.contacts[0].normal;
+        //Vector2 newVelocity = Vector2.Reflect(inDirection, inNormal);
+        //
+        //rb.velocity = newVelocity * vitesse;
+
+        // Vector3 newDir = new Vector3(transform.position.x, transform.position.y, 0).normalized;
+        // float newDirValue = Mathf.Atan2(newVelocity.y - currDir.y, newVelocity.x - currDir.x);
+        // float newDirValueDeg = (180 / Mathf.PI) * newDirValue;
+        // transform.rotation = Quaternion.Euler(0, 0, newDirValueDeg);
     }
 }
