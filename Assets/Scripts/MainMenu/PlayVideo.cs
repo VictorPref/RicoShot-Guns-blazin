4d23a3ef8bd19838209904d10a037251a9bd81a4
VictorPref <VictorPref@users.noreply.github.com>
2018-09-04 11:51:51 -0400

    Resolved merge conflict in Obstacle
    Resolved merge conflict in Obstacle
    
    Removed method initilization and changed line 22 in OnCollisionEnter2D

---
+++ Assets/Scripts/MainMenu/PlayVideo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour {
+
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audio;
+
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(Video());
+
+	}
+
+    IEnumerator Video()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audio.Play();
+    }
+}
